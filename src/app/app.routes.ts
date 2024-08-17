b93f1a7c81ce021175e3b6d42fae7dd68f777ddf
Kaushal Rai <kaushal.rai@gmail.com>
2024-08-16 22:43:11 -0400

    Added Bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path:'aboutUs', component:AboutusComponent}, 
+    {path:'contactUs',component:ContactUsComponent}
+
+];
